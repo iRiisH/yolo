e3b9eb5e80c22106d3316128b9b2ba7e1465b8a2
Datoclement <70664914@qq.com>
2017-01-07 23:39:13 +0100

    cest marrant

--- buildyolo.py
+++ buildyolo.py
@@ -1,43 +1,99 @@
-########################################################################################
-# Davi Frossard, 2016                                                                  #
-# VGG16 implementation in TensorFlow                                                   #
-# Details:                                                                             #
-# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
-#                                                                                      #
-# Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
-# Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
-########################################################################################
+####
+# First version yolo copy
+# Based on pretrained model vvg16
 
 import tensorflow as tf
 import numpy as np
-from scipy.misc import imread, imresize
-from imagenet_classes import class_names
+
+class yolonet:
+
+    def __init__(data,vgg16_weights,sess):
+        self.data = data
+        self.vgg16model(data,vvg16_weights,sess)
+        self.deleteLayers()
+        self.addLayers()
+
+    def deleteLayers():
+        #TODO
+
+    def addLayers():
+
+        with tf.name_scope('conv_added1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                    stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.pool5, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                    stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added3') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                    stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added4') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                    stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added3, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                 trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        self.pool_added1 = tf.nn.max_pool(self.conv_added4,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME',
+                               name='pool_added1')
+
+        #TODO
+
+        self.pool_added2 = tf.nn.max_pool(self.dropout,
+                                ksize=[1, 2, 2, 1],
+                                strides=[1, 2, 2, 1],
+                                padding='SAME',
+                                name='pool_added2')
 
 
-class vgg16:
-    def __init__(self, imgs, weights=None, sess=None):
+    def vgg16model(self, imgs, weights=None, sess=None):
         self.imgs = imgs
         self.convlayers()
         self.fc_layers()
         self.probs = tf.nn.softmax(self.fc3l)
-        if weights is not None and sess is not None:
-            self.load_weights(weights, sess)
-
+        self.load_weights(weights, sess)
 
     def convlayers(self):
         self.parameters = []
 
+        #NOTE THAT WE NEED TO CHANGE HERE
         # zero-mean input
-        # with tf.name_scope('preprocess') as scope:
-        #     mean = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
-        #     images = self.imgs-mean
+        with tf.name_scope('preprocess') as scope:
+            mean = tf.constant([123.68, 116.779, 103.939], dtype=tf.float32, shape=[1, 1, 1, 3], name='img_mean')
+            images = self.imgs-mean
 
         # conv1_1
         with tf.name_scope('conv1_1') as scope:
             kernel = tf.Variable(tf.truncated_normal([3, 3, 3, 64], dtype=tf.float32,
                                                      stddev=1e-1), name='weights')
-            conv = tf.nn.conv2d(self.imgs, kernel, [1, 1, 1, 1], padding='SAME')
-            # conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+            conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
             biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32),
                                  trainable=True, name='biases')
             out = tf.nn.bias_add(conv, biases)
--- yolo.py
+++ yolo.py
@@ -2,18 +2,111 @@ import tensorflow as tf
 import numpy as np
 from vgg16.network import *
 from scipy.misc import imread, imresize
+from vgg16.imagenet_classes import *
+
+class yolonet:
+
+    def __init__(data,vgg16_weights,sess):
+        self.data = data
+        self.vgg16(data,vvg16_weights,sess)
+        self.deleteLayers()
+        self.addLayers()
+
+    def deleteLayers():
+        #TODO
+        return 
+
+    def addLayers(self):
+        with tf.name_scope('conv_added1') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.pool5, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added2') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added1, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added3') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added2, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('conv_added4') as scope:
+            kernel = tf.Variable(tf.truncated_normal([3,3,512,512],dtype=float32,
+                                stddev=1e-1),name='weights')
+            conv = tf.nn.conv2d(self.conv_added3, kernel, [1, 1, 1, 1], padding='SAME')
+            biases = tf.Variable(tf.constant(0.0, shape=[512], dtype=tf.float32),
+                                trainable=True, name='biases')
+            out = tf.nn.bias_add(conv, biases)
+            self.conv_added = tf.nn.relu(out,name=scope)
+            self.parameters += [kernel, biases]
+
+        with tf.name_scope('fc1') as scope:
+            shape = int(np.prod(self.conv_added4.get_shape()[1:]))
+            fc1w = tf.Variable(tf.truncated_normal([shape,4096],dtype=tf.float32,
+                                stddev=1e-1), name='weights')
+            fc1b = tf.Variable(tf.constant(1.0, shape=[4096],dtype=tf.float32),
+                                trainable=True, name='biases')
+            conv_added4_flat = tf.reshape(self.conv_added4, [-1, shape])
+            fc1l = tf.nn.bias_add(tf.matmul(conv_added4_flat, fc1w), fc1b)
+            self.fc1 = tf.nn.relu(fc1l)
+            self.parameters += [fc1w, fc1b]
+
+        #TODO self.dropout=tf.nn.dropout(self.fc1,)
+
+        with tf.name_scope('fc2') as scope:
+            shape = int(np.prod(self.dropout.get_shape()[1:]))
+            fc2w = tf.Variable(tf.truncated_normal([shape, 4096],dtype=tf.float32,
+                                stddev=1e-1), name='weights')
+            fc2b = tf.Variable(tf.constant(1.0, shape=[4096], dtype=tf.float32),
+                                trainable=True, name='biases')
+            dropout_flat = tf.reshape(self.dropout, [-1, shape])
+            fc2l = tf.nn.bias_add(tf.matmul(dropout_flat, fc2w), fc2b)
+            self.fc2 = tf.nn.relu(fc2l)
+            self.parameters += [fc2w, fc2b]
 
 if __name__ == '__main__':
+
+    #new code
+    # sess = tf.Session()
+    # #TODO how to change the size of inputs
+    # data = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    # network = yolonet(data,'vgg16/vgg16_weights.npz',sess)
+    #
+    # data_input = imread('laska.png', mode='RGB')
+    # data_input = imresize(data, (224, 224))
+    #
+    # #TODO is it the same for several images?
+    # prob = sess.run(network.probs, feed_dict={network.data: [data_input]})[0]
+    # preds = (np.argsort(prob)[::-1])[0:5]
+    # for p in preds:
+    #     print class_names[p], prob[p]
+
     sess = tf.Session()
     imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
     network = vgg16(imgs, 'vgg16/vgg16_weights.npz', sess)
-
 	# TODO modify network in order to implement YOLO neural network
 
     img1 = imread('laska.png', mode='RGB')
     img1 = imresize(img1, (224, 224))
 
-    prob = sess.run(vgg.probs, feed_dict={vgg.imgs: [img1]})[0]
+    prob = sess.run(network.probs, feed_dict={network.imgs: [img1]})[0]
     preds = (np.argsort(prob)[::-1])[0:5]
     for p in preds:
         print class_names[p], prob[p]
