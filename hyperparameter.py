8c661c9b6eb497a924d73ac4a8a66d5ae492fff4
iRiisH <vzs@hotmail.fr>
2017-03-04 14:35:40 +0100

    new hyperparameters

--- hyperparameter.py
+++ hyperparameter.py
@@ -3,21 +3,21 @@ from tensorflow import flags
 def define_flags():
 
     flags.DEFINE_string("load_from_vgg16", True, "decide whether load from vgg16 model or previously trained model")
-
     flags.DEFINE_string("cfg_address", "cfg.txt", "address of the file for the cfg")
-
     flags.DEFINE_string("last_vgg16_layer", "pool5", "name of last vgg16 layer")
-
     flags.DEFINE_string("action", "train", "to train, to predict, or ?")
-
     # flags.DEFINE_string("number_of_cases", 7, "decide how we divide the pictures")
-
     flags.DEFINE_string("vgg16_weights_file", "vgg16/vgg16_weights.npz", "path to vgg16 weights file")
-
     flags.DEFINE_string("batch_size", 64, "the size of batches (the number of pictures taken in each training circle)")
-
     flags.DEFINE_string("classes_file", "classes_file.txt", "the path to the file that contains classes' names")
-
     flags.DEFINE_string("ann_directory", "Annotations", "the path to the file that contains classes' names")
-
+    flags.DEFINE_string ("object_scale", 5, "" )
+	flags.DEFINE_string ("noobject_scale", 1, "")
+	flags.DEFINE_string ("class_scale", 1, "")
+	flags.DEFINE_string("coord_scale", 1, "")
+	flags.DEFINE_string ("absolute", 1, "")
+	flags.DEFINE_string ("thresh", 0.3, "")
+	flags.DEFINE_string ("random", 0, "") 
+	
     return flags
+
