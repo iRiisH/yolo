363bc196fef3b6e330665ad171c769e88af847ae
iRiisH <vzs@hotmail.fr>
2017-03-06 20:42:13 +0100

    load function

--- save.py
+++ save.py
@@ -9,8 +9,13 @@ def _save_ckpt(self, step, loss_profile):
     with open(profile, 'wb') as profile_ckpt: 
         pickle.dump(loss_profile, profile_ckpt)
 
-    ckpt = file.format(model, step, '')
-    ckpt = os.path.join(self.FLAGS.backup, ckpt)
+    ckpt = filename.format(model, step, '')
+    ckpt = os.path.join(os.getcwd (), ckpt)
     self.say('Checkpoint at step {}'.format(step))
     self.saver.save(self.sess, ckpt)
 
+def _load_ckpt(filename): # not to be opened in an existing session
+	with tf.Session () as sess :
+		saver = tf.train.Saver()
+		saver.restore (sess, filename)
+	
